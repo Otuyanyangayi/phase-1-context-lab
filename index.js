74bc804065dfbaef24d12d1d9ebecb597c3ec2b5
Grace <gracenyangayi@gmail.com>
2022-06-19 12:45:44 +0300

    commit tree 0b610d05988893f62c33b5a36efcd757aaa5d6fc

--- index.js
+++ index.js
@@ -1,4 +1,79 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrays){
+    let employees = []
+    for(const array of arrays){
+        employees.push(createEmployeeRecord(array))
+    }
+    return employees
+}
+
+function createTimeInEvent(timeStamp) {
+    const date = timeStamp.split(" ")[0]
+    const hour = parseInt(timeStamp.split(" ")[1])
+    let obj = {
+        type: "TimeIn",
+        hour: hour,
+        date: date
+    }
+    this["timeInEvents"].push(obj)
+    return this
+}
+
+function createTimeOutEvent(timeStamp) {
+    const date = timeStamp.split(" ")[0]
+    const hour = parseInt(timeStamp.split(" ")[1])
+    let obj = {
+        type: "TimeOut",
+        hour: hour,
+        date: date
+    }
+    this["timeOutEvents"].push(obj)
+    return this
+}
+
+function getHours(array, date){
+    for(let i=0; i<array.length; i++) {
+        if(array[i].date === date){
+            return array[i].hour
+        }
+    }
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = getHours(this.timeInEvents, date)
+    const timeOut = getHours(this.timeOutEvents, date)
+    return (timeOut - timeIn) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    const rate = this["payPerHour"]
+    return hoursWorked * rate
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function(employee){
+        return employee.firstName === firstName
+    })
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
